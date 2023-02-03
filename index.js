0b5209d4771602f063e95d20d32fb8398bcba884
Gracie <graciengainey@gmail.com>
2023-02-03 09:55:21 -0500

    finished whackamole, works

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 let score = 0;
+const scoreDisplay = document.getElementById('score')
+const holes = document.getElementsByClassName("hole")
+
+setInterval(function molePopUp() {
+    const randomHoleIndex = Math.floor(Math.random() * holes.length);
+    holes[randomHoleIndex].classList.toggle("mole")}, 500)
+
+const gameArea = document.getElementById('whack-a-mole');
+gameArea.addEventListener('click', function(clickEvent) {
+    if (clickEvent.target.matches('.mole')) {
+        score ++;
+        clickEvent.target.classList.remove('mole');
+        scoreDisplay.innerText = score;
+    }
+})
